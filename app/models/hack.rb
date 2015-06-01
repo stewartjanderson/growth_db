70d892b3dbeaed2943340210b4a8ef40534ec450
stewartjanderson <stewartjanderson@gmail.com>
2015-06-01 00:25:24 -0700

    Modified index layout

--- app/models/hack.rb
+++ app/models/hack.rb
@@ -1,3 +1,8 @@
 class Hack < ActiveRecord::Base
 	belongs_to :user
+
+	validates :user_id, presence: true
+	validates :title, presence: true
+	validates :content, presence: true
+	validates :category, presence: true
 end
